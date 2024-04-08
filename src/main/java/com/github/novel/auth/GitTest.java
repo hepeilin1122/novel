79ac84d644e006871d639676d9c9ccfc63e9c744
hepeilin <hepeilin1122@163.com>
2024-04-08 12:31:15 +0800

    第二次修改

--- src/main/java/com/github/novel/auth/GitTest.java
+++ src/main/java/com/github/novel/auth/GitTest.java
@@ -3,5 +3,6 @@ package com.github.novel.auth;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试修改");
+        System.out.println("第二次修改");
     }
 }
