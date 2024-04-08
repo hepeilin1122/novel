ad74eb201be525e6c218d1b8984260b39ef40a8d
hepeilin <hepeilin1122@163.com>
2024-04-08 12:56:14 +0800

    三次修改

--- src/main/java/com/github/novel/auth/GitTest.java
+++ src/main/java/com/github/novel/auth/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试修改");
         System.out.println("第二次修改");
+        System.out.println("合并到dev");
     }
 }
